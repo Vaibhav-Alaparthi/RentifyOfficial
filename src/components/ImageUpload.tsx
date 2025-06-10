296f8b79a7dd321b4f509831ae57d97568eb5f3c
Vaibhav Alaparthi <67380585+Vaibhav-Alaparthi@users.noreply.github.com>
2025-06-09 21:21:47 -0700

    Update ImageUpload.tsx

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,6 +1,6 @@
 /*
   Ayush Vupalanchi, Vaibhav Alaparthi, Hiruna Devadithya
-  6/9/25
+  6/4/25
 
   This file provides a component for users to upload, preview, and remove images for a rental listing in the renting marketplace website.
 */
